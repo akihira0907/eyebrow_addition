51377c15ea8335a189aa284dc4ad00c6944489a5
akihira0907 <akihira0907starriver@gmail.com>
2019-11-25 23:01:25 +0900

    文字化け修正

--- eybrow_addiction.py
+++ eybrow_addiction.py
@@ -5,33 +5,33 @@ import numpy as np
 import csv
 
 def main(img_path, landmark_path):
-    # $B2hA|FI$_9~$_(B
+    # 画像読み込み
     img = cv2.imread(img_path)
     if img is None:
         print("Failed to load image.")
 
-    # Landmark$BFI$_9~$_(B
+    # Landmark読み込み
     csv_file = open(landmark_path)
     reader = csv.reader(csv_file)
     l = [row for row in reader]
-    # str$B$+$i(Bfloat$B$X(B,float$B$+$i(Bint$B$X(B
+    # strからfloatへ,floatからintへ
     landmarks_x = list(map(float, l[1][296:364]))
     landmarks_y = list(map(float, l[1][364:432]))
     landmarks_x = list(map(int, landmarks_x))
     landmarks_y = list(map(int, landmarks_y))
 
-    # $BH}LS$N%i%s%I%^!<%/<hF@(B
+    # 眉毛のランドマーク取得
     eyebrows_landmarks = []
     for i in range(17, 27):
         eyebrows_landmarks.append((landmarks_x[i], landmarks_y[i]))
     print(eyebrows_landmarks)
 
-    # $B;XDj$5$l$?%T%/%;%k$N<~0O$r9u$/$9$k(B(3ch$B$"$k$3$H$KCm0U(B)
+    # 指定されたピクセルの周囲を黒くする(3chあることに注意)
     size = 20
     for x, y in eyebrows_landmarks:
         img[y-size:y+size, x-size:x+size, :] = 0
 
-    # $B2hA|=q$-=P$7(B
+    # 画像書き出し
     output_filename = "output.jpg"
     ret = cv2.imwrite(output_filename, img)
     if not ret:
